7b9e2f7ff6c9ed287845d16ba2054f3e56491c8f
Edson Gomes <biduzao.bidu21@gmail.com>
2025-08-25 17:21:59 -0300

    initial login

--- LoginWeb/Program.cs
+++ LoginWeb/Program.cs
@@ -1,4 +1,5 @@
 using LoginWeb;
+using LoginWeb.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<FingerprintService>();
+
+// Removed HttpClientHandler.UseCookies as it is not supported in the browser environment.  
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5100"),
+    DefaultRequestVersion = new Version(2, 0)
+});
 
 await builder.Build().RunAsync();
